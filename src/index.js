2e5f7ddb8936492f626f8c7e11efa4e4de2580b3
flashjaysan <jaysan@wanadoo.fr>
2021-12-16 18:42:55 +0100

    access gateway every minute

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const schedule = require('node-schedule');
+const axios = require('axios');
+
+const gatewayUrl = `http://app.objco.com:8099/?account=NTB37PKZUG&limit=10`;
+
+const getData = function() {
+  axios.get(gatewayUrl)
+      .then(function(response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function() {
+        // always executed
+      });
+};
+
+schedule.scheduleJob('*/1 * * * *', getData);
